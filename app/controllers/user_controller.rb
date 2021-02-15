bb566a2c22dcfe4197400d296bed684dd719629c
weiyeegoh <weiyeeg@gmail.com>
2021-02-14 18:42:38 -0800

    added an example controller reference of user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,31 @@
 class UserController < ApplicationController
 
   def index
+    @user = User.all
+    render json: {users: @user}
+  end
 
-    render json: {status: 200}
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  ### NOTE: Just a hardcoded example, will need to remove hard coded values when fields are set as unique
+  def create
+
+  	@is_auth = true
+  	@name = "weiyee"
+  	@username = "weiyee123123"
+  	@password = "123123"
+
+    @user = User.new(is_auth: @is_auth, name:@name, username:@username, password:@password)
+    if @user.save
+      render json: {user: @user}
+    else
+      render json: {status: 200}
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   get 'user', to: 'user#index'
+  get 'signup', to: 'user#create'
 
 end
