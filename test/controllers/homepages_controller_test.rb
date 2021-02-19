6c6dd3cc935d1e2b3b82dd106bcbf0a7f0f3c54f
WeiyeeGoh <WeiyeeGoh@users.noreply.github.com>
2021-02-19 00:47:11 -0800

    Wy clean duplicate controllers (#38)
    * updated database schema for members because user_id and room_id was not properly tied to user and room table
    
    * removed member_controller and transferred code from there to members_controller
    
    * removed room_controller because rooms_controller already exists
    
    * changed user controller to plural
    
    * changed hompage controller name from singular to plural
    
    * cleanup misspelled HomepageController in homepage_controller.rb and guest login title typo

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class RoomControllerTest < ActionDispatch::IntegrationTest
+class HomepagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
