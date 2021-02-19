841230fffd8c18d5c4ded5ff71220066810580db
weiyeegoh <weiyeeg@gmail.com>
2021-02-19 00:30:49 -0800

    removed member_controller and transferred code from there to members_controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,16 @@
 class MembersController < ApplicationController
+    protect_from_forgery :except => :create 
+
+    def index
+        @member = Member.all
+        render json: @member 
+    end
+
+    def show
+        @member = Member.find(params[:id])
+        render json: @member
+    end
+
     def create
         @rooms_id = params[:rooms_id]
         @users_id = params[:users_id]
@@ -10,6 +22,20 @@ class MembersController < ApplicationController
             render json: {member: @member}
         else
             render json: {status: 500}
+        end
+    end
+
+    def member_params
+        params.require(:member).permit(:user_id, :room_id, :is_host)
+    end
+    
+    def destroy 
+        if Member.exists? id: params[:id]
+            @member = Member.find(params[:id])
+            @member.destroy
+            render json: @member
+        else
+            render json: {status: 404}
         end
     end
 end
