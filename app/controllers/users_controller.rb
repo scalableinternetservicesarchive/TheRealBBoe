96b8a869eeb21ae914115693d49d206e94484965
aprilsanchez <sapril338@gmail.com>
2021-03-04 00:10:36 -0800

    Only /get_rooms if user is authenticated, not a guest user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
             @user.is_auth = user_params.has_key?(:password)	
             @user.save
             session[:user_id] = @user.id
-            render json: {user_data: {id: @user.id, name: @user.name, username: @user.username}}, status: 201
+            render json: {user_data: {id: @user.id, name: @user.name, username: @user.username, is_auth: @user.is_auth}}, status: 201
         end
     end	
 
