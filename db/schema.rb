20e1db8ca365d93d459dd1c1bf41fe260e3a9770
laboni68 <labonisarker15@gmail.com>
2021-03-12 04:13:49 -0800

    schema and test file changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_115102) do
+ActiveRecord::Schema.define(version: 2021_03_12_120402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2021_03_12_115102) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.index ["username"], name: "index_users_on_username"
   end
 
   add_foreign_key "restaurants", "locations"
