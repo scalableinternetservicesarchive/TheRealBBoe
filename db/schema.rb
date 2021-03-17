f63c6e7a1b53769facb88696edeb2609484f134d
rx5842 <ryan8xia@gmail.com>
2021-03-17 15:24:10 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -43,20 +43,22 @@ ActiveRecord::Schema.define(version: 2021_03_10_050838) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.string "token"
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "location_id"
+    t.string "token"
+    t.index ["token"], name: "index_rooms_on_token"
   end
 
   create_table "users", force: :cascade do |t|
     t.boolean "is_auth"
-    t.string "username"
     t.string "name"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.index ["username"], name: "index_users_on_username"
   end
 
   add_foreign_key "restaurants", "locations"
